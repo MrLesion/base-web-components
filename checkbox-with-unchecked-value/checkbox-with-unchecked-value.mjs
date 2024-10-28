21555ec9ed4c0f0a2cb5ad560e0d7ab7a9577a70
damngood_dk <jd@co3.dk>
2024-10-28 13:21:04 +0100

    add support for checkbox list + cleaning up

--- checkbox-with-unchecked-value/checkbox-with-unchecked-value.mjs
+++ checkbox-with-unchecked-value/checkbox-with-unchecked-value.mjs
@@ -1,63 +1,44 @@
 import CustomElementBase from '../custom-element-base.mjs';
 
 class CheckboxWithUncheckedValue extends CustomElementBase {
-	static tagName = 'checkbox-with-unchecked-value';
-	static customEventPrefix = this.tagName + ':';
-	static classes = {}
-	static selectors = {
-		inputCheckbox: 'input[type="checkbox"]',
-	};
-	static attributes = {
-		value: 'value'
-	};
-	static properties = {
-		value: 'value'
-	};
-	static events = {
-		change: 'change'
-	};
-	static observedAttributes = [];
-	static observedProperties = [this.properties.value];
-	static observedEvents = [this.events.change];
 
-	constructor() {
-		super();
+  static tagName = 'checkbox-with-unchecked-value';
+  static customEventPrefix = this.tagName + ':';
+  static selectors = {
+    inputCheckbox: 'input[type="checkbox"]',
+    inputCheckboxChecked: 'input[type="checkbox"]:checked',
+  };
+  static properties = {
+    'unchecked-value': 'unchecked-value'
+  };
+  static events = {
+    change: 'change'
+  };
+  static observedProperties = [this.properties['unchecked-value']];
+  static observedEvents = [this.events.change];
 
-		this.inputCheckbox = this.querySelector(this.constructor.selectors.inputCheckbox);
-		this.inputValue = this.inputCheckbox.insertAdjacentElement('afterend', document.createElement('input'));
-		if (this.inputValue !== null) {
-			this.inputValue.name = this.inputCheckbox.name;
-			this.inputCheckbox.removeAttribute('name');
-			this.inputValue.type = 'hidden';
-			this.inputValue.value = this.inputCheckbox.checked;
-		}
-		this.value = this.inputValue.value
-	};
+  constructor() {
+    super();
 
-	eventHandlers = {
-		[this.constructor.events.change]: (objEvent) => {
-			this.inputValue.value = this.inputCheckbox.checked;
-			console.log('eventHandlers', objEvent.type, this.inputValue.value, this.inputCheckbox.checked)
-		}
-	};
-	attributeHandlers = {};
-	propertyHandlers = {
-		[this.constructor.properties.value]: {
-			get: (strPropertyName) => {
-				const strReturnValue = this.getAttribute(strPropertyName);
-				console.log('prop getter with custom handler', strPropertyName, strReturnValue);
-				return strReturnValue;
-			},
-			set: (value) => {
-				console.log('prop setter with custom handler', this.constructor.properties.value, value);
-				const strValue = String(value) ?? '';
-				this.setAttribute(this.constructor.properties.value, strValue);
-			}
-		}
-	};
+    this.inputCheckbox = this.querySelector(this.constructor.selectors.inputCheckbox);
+    this.inputUncheckedValue = this.insertAdjacentElement('beforeend', document.createElement('input'));
+    if (this.inputUncheckedValue !== null) {
+      this.inputUncheckedValue.name = this.inputCheckbox.name;
+      this.inputUncheckedValue.type = 'hidden';
+      this.inputUncheckedValue.value = this[this.constructor.properties["unchecked-value"]] || 'default to empty string';
+      this.triggerEvent(this.constructor.events.change, this.inputCheckbox);
+    }
+  };
+
+  eventHandlers = {
+    [this.constructor.events.change]: (objEvent) => {
+      this.inputUncheckedValue.disabled = this.querySelector(this.constructor.selectors.inputCheckboxChecked) !== null;
+      console.log('eventHandlers', objEvent.type, this.constructor.selectors.inputCheckboxChecked, this.querySelector(this.constructor.selectors.inputCheckboxChecked), this.inputUncheckedValue)
+    }
+  };
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-	console.log('window DOMContentLoaded');
-	CheckboxWithUncheckedValue.define();
+  console.log('window DOMContentLoaded');
+  CheckboxWithUncheckedValue.define();
 });
